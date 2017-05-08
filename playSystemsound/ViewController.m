22f7f0aadc605aeb10418e0735505563c1ed2cee
zhangyoubo <13818956024@163.com>
2017-05-08 15:29:05 +0800

    2017.5.8

--- playSystemsound/ViewController.m
+++ playSystemsound/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <AudioToolbox/AudioToolbox.h>
 @interface ViewController ()
 
 @end
@@ -17,9 +17,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UIButton*btn=[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [btn setTitle:@"播放" forState:UIControlStateNormal];
+    btn.frame=CGRectMake(110, 300, 100, 40);
+    [btn addTarget:self action:@selector(playSound) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)playSound{
+    //系统声音
+     AudioServicesPlaySystemSound(1007);
+    
+    //震动
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
